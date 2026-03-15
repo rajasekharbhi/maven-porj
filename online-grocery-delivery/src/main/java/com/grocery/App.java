f09bd9aefc50c2162732b266cc862eee4272f591
rajasekharbhi <84693973+rajasekharbhi@users.noreply.github.com>
2026-03-15 22:22:01 +0530

    Update App.java

--- online-grocery-delivery/src/main/java/com/grocery/App.java
+++ online-grocery-delivery/src/main/java/com/grocery/App.java
@@ -1,13 +1,11 @@
 package com.grocery;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Online Grocery Delivery System Started");
+    }
+
+    public String getWelcomeMessage() {
+        return "Welcome to Online Grocery Delivery!";
     }
 }
