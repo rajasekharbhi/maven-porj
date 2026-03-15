37e9388f9f1d5e2864aa488c795cc20a878dfe82
rajasekharbhi <84693973+rajasekharbhi@users.noreply.github.com>
2026-03-15 22:23:08 +0530

    Create AppTest.java

---
+++ online-grocery-delivery/src/main/java/com/grocery/AppTest.java
@@ -0,0 +1,12 @@
+package com.grocery;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    @Test
+    public void testWelcomeMessage() {
+        App app = new App();
+        assertEquals("Welcome to Online Grocery Delivery!", app.getWelcomeMessage());
+    }
+}
