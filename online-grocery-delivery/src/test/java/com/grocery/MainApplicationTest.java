7cea61d82949289389447dcc2d520e4351ad8b57
rajasekharbhi <84693973+rajasekharbhi@users.noreply.github.com>
2026-03-17 21:12:41 +0530

    Update MainApplicationTest.java

--- online-grocery-delivery/src/test/java/com/grocery/MainApplicationTest.java
+++ online-grocery-delivery/src/test/java/com/grocery/MainApplicationTest.java
@@ -3,10 +3,9 @@ package com.grocery;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class AppTest {
+public class MainApplicationTest {
     @Test
     public void testWelcomeMessage() {
-        // Change 'App' to 'MainApplication'
         MainApplication app = new MainApplication();
         assertEquals("Welcome to Online Grocery Delivery!", app.getWelcomeMessage());
     }
